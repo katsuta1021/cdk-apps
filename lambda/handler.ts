1895d0952fe65e2807e6c95eee5d481c966b2f5a
root <root@DESKTOP-6FAGUF2.>
2025-07-04 10:18:25 +0900

    エンドポイント追加

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -3,7 +3,11 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 export const handler: APIGatewayProxyHandler = async () => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST"
+    },
     body: JSON.stringify({ message: "投稿成功" }),
-    headers: { "Access-Control-Allow-Origin": "*" }
   };
 };
